f545e14fbcd87b681c26b96ef30ea1ec478f4ec4
jhoun <jayhoun@gmail.com>
2017-02-16 11:05:06 -1000

    implemented js injection into news article

--- contentScript.js
+++ contentScript.js
@@ -2,10 +2,54 @@
 
 // chrome.runtime.sendMessage(document.URL);
 
+//bbc
 if (document.domain === "www.bbc.com") {
   chrome.runtime.sendMessage(
     document.querySelector('meta[property="og:description"]').content
   );
+
+  // just place a div at top right
+  var body = document.getElementsByClassName('body-content');
+  console.log('body: ', body);
+  var div = document.createElement('div');
+  div.style.fontSize = '2em';
+  div.textContent = 'Injected!';
+  div.style.backgroundColor = 'green';
+  // div.style.width = 100px;
+  // div.style.height = 100px;
+  body[0].appendChild(div);
+
+  alert('inserted');
+
 }
+
+//reuters
+if (document.domain === "www.reuters.com") {
+  chrome.runtime.sendMessage(
+    document.querySelector('meta[property="og:description"]').content
+  );
+
+
+
+  // just place a div at top right
+  var body = document.getElementById('article-text');
+  console.log('body: ', body);
+  var div = document.createElement('div');
+  div.style.fontSize = '2em';
+  div.textContent = 'Injected!';
+  div.style.backgroundColor = 'green';
+  // div.style.width = 100px;
+  // div.style.height = 100px;
+  body.appendChild(div);
+
+  alert('inserted self... giggity');
+
+
+}
+
+
+
+
+
 
 console.log('hello')
