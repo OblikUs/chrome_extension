f0f739f9b722c5f0fd0ddc5a6e640fd4fd197fd9
jhoun <jayhoun@gmail.com>
2017-02-16 16:14:02 -1000

    implemented page url to server scraper

--- background.js
+++ background.js
@@ -2,12 +2,6 @@ chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
   alert(response);
 })
 
-// chrome.browserAction.onClicked.addListener(function (tab) {
-//   // for the current tab, inject the "inject.js" file & execute it
-//   console.log('hit');
-//   chrome.tabs.executeScript(tab.ib, {
-//     file: 'contentScript.js'
-//   });
-// });
 
-console.log('hazza');
+
+
