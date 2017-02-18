a801187101bee6950814d5fedcf7c19ff82b0be4
jhoun <jayhoun@gmail.com>
2017-02-17 19:58:16 -1000

    implemented a pop up after onclick icon

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-  alert(response);
-})
 
-
-
-
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    //Sends message to contentScript.js
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
