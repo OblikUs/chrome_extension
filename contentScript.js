e08551ddbb2d7734260832777005e9d1b19e435c
jhoun <jayhoun@gmail.com>
2017-02-24 12:32:25 -1000

    formated article containers

--- contentScript.js
+++ contentScript.js
@@ -6,19 +6,21 @@ xhr.setRequestHeader("content-type", "application/json; charset=UTF-8");
 xhr.send(JSON.stringify(url));
 
 
-xhr.addEventListenter("load", getData);
-xhr.open('GET', 'https://localhost:8080/', true);
-oreq.send()
-
-function getData() {
-  console.log('this.responseText: ', this.responseText);
-}
 
 //listens from background.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message === "first_click") {
       //html tag
+      xhr.addEventListener("load", getData);
+      xhr.open('GET', 'https://localhost:8080/api/popup', true);
+      xhr.send()
+
+      function getData() {
+        let article = JSON.parse(this.responseText);
+        console.log('article: ', article.results[0].data[1].row[0].title);
+      }
+
       let html = document.getElementsByTagName('html')[0];
 
       //div popupContainer
@@ -59,7 +61,7 @@ chrome.runtime.onMessage.addListener(
             //div title child of modalHeader
             let title = document.createElement('div');
             title.classList.add('titleee')
-            title.innerHTML = "obliq";
+            title.innerHTML = "oblik";
             modalHeader.appendChild(title);
 
           //div modalBody child of modalContent
@@ -67,20 +69,49 @@ chrome.runtime.onMessage.addListener(
           modalBody.classList.add('modal-bodyyy');
           modalContent.appendChild(modalBody);
 
-            //div article1Container child of modalContent
-            let article1Container = document.createElement('div');
-            article1Container.classList.add('article1Container');
-            modalContent.appendChild(article1Container);
+            //div article source1
+            let articleSource1 = document.createElement('div');
+            articleSource1.classList.add('articleSource1');
+            articleSource1.innerHTML = "FOX NEWS";
+            modalBody.appendChild(articleSource1);
+
+              //div article1Container child of modalContent
+              let article1Container = document.createElement('div');
+              article1Container.classList.add('article1Container');
+              articleSource1.appendChild(article1Container);
+
+            //div article source 2
+            let articleSource2 = document.createElement('div');
+            articleSource2.classList.add('articleSource2');
+            articleSource2.innerHTML = "REUTERS";
+            modalBody.appendChild(articleSource2);
+
+              //div article2Container child of modalContent
+              let article2Container = document.createElement('div');
+              article2Container.classList.add('article2Container');
+              articleSource2.appendChild(article2Container);
+
+            //div article source 3
+            let articleSource3 = document.createElement('div');
+            articleSource3.classList.add('articleSource3');
+            articleSource3.innerHTML = "HUFFINGTING POST";
+            modalBody.appendChild(articleSource3);
+
+              //div article3Container child of modalContent
+              let article3Container = document.createElement('div');
+              article3Container.classList.add('article3Container');
+              articleSource3.appendChild(article3Container);
+
 
             //onclick redirects to link
             article1Container.onclick = function() {
               chrome.runtime.sendMessage({redirect: "http://google.com"});
             }
 
-          //div modalFooter child of modalContent
-          let modalFooter = document.createElement('div');
-          modalFooter.classList.add('modal-footerrr');
-          modalContent.appendChild(modalFooter);
+          // //div modalFooter child of modalContent
+          // let modalFooter = document.createElement('div');
+          // modalFooter.classList.add('modal-footerrr');
+          // modalContent.appendChild(modalFooter);
 
       // click anywhere on the window to exit
       window.onclick = function(event) {
